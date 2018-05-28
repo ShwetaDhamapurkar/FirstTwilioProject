d527ac3dd6b1c35b7b224556c88e9f31870063e5
Shweta Dhamapurkar <Shweta.Dhamapurkar@gmail.com>
2018-05-27 17:19:47 -0700

    Updating Phone number,Account_SID,AUTH_TOKEN as its private info

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -4,14 +4,14 @@ import com.twilio.type.PhoneNumber;
 
 public class Example {
     // Find your Account Sid and Token at twilio.com/user/account
-    public static final String ACCOUNT_SID = "AC72386f483cdf669fad08edb47628e034";
-    public static final String AUTH_TOKEN = "d3eb414f67133338e4164ab8c69c072d";
+    public static final String ACCOUNT_SID = "ACCOUNT_SID";
+    public static final String AUTH_TOKEN = "AUTH_TOKEN";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        Message message = Message.creator(new PhoneNumber("+12134005710"),
-                new PhoneNumber("+14242871781"),
+        Message message = Message.creator(new PhoneNumber("+1xxxxxxxxxx"),
+                new PhoneNumber("+1xxxxxxxxxx"),
                 "This is the ship that made the Kessel Run in fourteen parsecs?").create();
 
         System.out.println(message.getSid());
